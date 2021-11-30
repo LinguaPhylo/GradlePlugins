aaf7daab41ed7f8f16d3cedd39ea3364384ad565
walterxie <walter@cs.auckland.ac.nz>
2021-11-30 14:06:18 +1300

    better implementation of publish and add package-info

--- platforms/src/main/java/io/github/linguaphylo/platforms/LPhyPublishPlugin.java
+++ platforms/src/main/java/io/github/linguaphylo/platforms/LPhyPublishPlugin.java
@@ -1,13 +1,15 @@
 package io.github.linguaphylo.platforms;
 
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.tasks.PublishToMavenRepository;
-import org.gradle.api.publish.plugins.PublishingPlugin;
 import org.gradle.authentication.http.BasicAuthentication;
 import org.gradle.plugins.signing.SigningExtension;
 import org.gradle.plugins.signing.SigningPlugin;
@@ -16,8 +18,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * Define maven publishing conventions.
- * Require signing and maven-publish plugins.
+ * This is inspired from Gradle plugin code
+ * {@code org.gradle.plugin.devel.plugins.MavenPluginPublishPlugin}.
+ *
+ * It defines maven publishing conventions,
+ * and requires signing and maven-publish plugins.
  * Java conventions are defined separately.
  * @see LPhyJavaPlugin
  *
@@ -44,69 +49,25 @@ public class LPhyPublishPlugin implements Plugin<Project> {
         boolean isSigning = project.hasProperty("signing.password")
                 && project.hasProperty("signing.keyId");
 
-        // config repos
-        project.getPlugins().withType(PublishingPlugin.class, pubPlugin -> {
-            PublishingExtension pubExt = project.getExtensions().getByType(PublishingExtension.class);
-            /* repositories {
-              maven {
-                name = "maven"
-                val ossrhUser = findProperty("ossrh.user")
-                val ossrhPswd = findProperty("ossrh.pswd")
-                credentials {
-                    username = "$ossrhUser"
-                    password = "$ossrhPswd"
-                }
-                authentication {
-                    create<BasicAuthentication>("basic")
-                }
-                url = ...
-              maven {
-                name = "local"
-                url = uri(layout.buildDirectory.dir("releases"))
-              }
-            } */
-            pubExt.repositories(repo -> {
-                if (hasOSSRHCredentials) {
-                    String ossrhUser = String.valueOf(project.property(OSSRH_USER));
-                    String ossrhPswd = String.valueOf(project.property(OSSRH_PSWD));
-                    // publish to maven central
-                    repo.maven(mvn -> {
-                        mvn.setName(PUBLISH_TO_MAVEN_CENTRAL);
-                        mvn.credentials(cred -> {
-                            cred.setUsername(ossrhUser);
-                            cred.setPassword(ossrhPswd);
-                        });
-                        mvn.authentication(auth -> {
-                            auth.create("basic", BasicAuthentication.class);
-                        });
-
-                        try {
-                            // set to SNAPSHOT as default, update in tasks.withType(PublishToMavenRepository.class)
-                            mvn.setUrl(new URI(OSS_SNAPSHOT));
-                        } catch (URISyntaxException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } else {
-                    // if "ossrh.user" not provided, then publish to local
-                    repo.maven(mvn -> {
-                        mvn.setName(PUBLISH_TO_LOCAL);
-                        DirectoryProperty buildDir = project.getLayout().getBuildDirectory();
-                        mvn.setUrl(buildDir.dir(LOCAL_RELEASE_DIR));
-                    });
-                }
-            });
-
+        project.afterEvaluate(new Action<Project>() {
+            @Override
+            public void execute(final Project project) {
+                // config repos
+                configurePublishingRepos(project, hasOSSRHCredentials);
+            }
         });
 
 
         // update url, and signing MavenPublication
-        project.getTasks().withType(PublishToMavenRepository.class)
-                .configureEach(pubTo -> {
-                    pubTo.doFirst(task -> {
-                        // project.getVersion() not working in config
-                        boolean isReleaseVersion = !project.getVersion().toString().endsWith(STR_SNAPSHOT);
-                        MavenArtifactRepository repo = pubTo.getRepository();
+        project.getTasks().withType(PublishToMavenRepository.class).configureEach(new Action<PublishToMavenRepository>() {
+            // project.getVersion() not working in config
+            final boolean isReleaseVersion = !project.getVersion().toString().endsWith(STR_SNAPSHOT);
+            @Override
+            public void execute(PublishToMavenRepository pubToMaven) {
+                MavenArtifactRepository repo = pubToMaven.getRepository();
+                pubToMaven.doFirst(new Action<Task>() {
+                    @Override
+                    public void execute(Task task) {
                         // if repo name sets to "maven" and is release version, then use deploy url
                         if (PUBLISH_TO_MAVEN_CENTRAL.equalsIgnoreCase(repo.getName())) {
                             if (isReleaseVersion) {
@@ -118,21 +79,85 @@ public class LPhyPublishPlugin implements Plugin<Project> {
                             }
                         }
 
-                        /* signing { sign(publishing.publications.matching{ it!!.name.toLowerCase().contains("lphy") }) } */
+                        /* signing {
+                             sign(publishing.publications.matching{ it!!.name.toLowerCase().contains("lphy") })
+                           } */
                         // if signing properties are provided
                         if (isSigning) {
                             project.getPlugins().apply(SigningPlugin.class);
                             SigningExtension signExt = project.getExtensions().getByType(SigningExtension.class);
 
-                            MavenPublication mavPub = pubTo.getPublication();
+                            MavenPublication mavPub = pubToMaven.getPublication();
                             System.out.println("Signing MavenPublication " + mavPub.getName());
-                            signExt.sign( mavPub );
+                            signExt.sign(mavPub);
                         }
-                        System.out.println("Publishing the " + (isReleaseVersion?"release":STR_SNAPSHOT.toLowerCase()) +
+                        System.out.println("Publishing the " + (isReleaseVersion ? "release" : STR_SNAPSHOT.toLowerCase()) +
                                 " version " + project.getVersion());
-                    });
+                    }
+                });
+            }
+        });
+    }
+
+    /* repositories {
+       maven {
+         name = "maven"
+         val ossrhUser = findProperty("ossrh.user")
+         val ossrhPswd = findProperty("ossrh.pswd")
+         credentials {
+             username = "$ossrhUser"
+             password = "$ossrhPswd"
+         }
+         authentication {
+             create<BasicAuthentication>("basic")
+         }
+         url = ...
+       maven {
+         name = "local"
+         url = uri(layout.buildDirectory.dir("releases"))
+       }
+    } */
+    private void configurePublishingRepos(final Project project, final boolean toOSSRH) {
+        project.getExtensions().configure(PublishingExtension.class, new Action<PublishingExtension>() {
+            @Override
+            public void execute(PublishingExtension pubExt) {
+                pubExt.repositories(new Action<RepositoryHandler>() {
+                    @Override
+                    public void execute(RepositoryHandler repoHandler) {
+                        if (toOSSRH) {
+                            String ossrhUser = String.valueOf(project.property(OSSRH_USER));
+                            String ossrhPswd = String.valueOf(project.property(OSSRH_PSWD));
+                            // publish to maven central
+                            repoHandler.maven(mvn -> {
+                                mvn.setName(PUBLISH_TO_MAVEN_CENTRAL);
+                                mvn.credentials(cred -> {
+                                    cred.setUsername(ossrhUser);
+                                    cred.setPassword(ossrhPswd);
+                                });
+                                mvn.authentication(auth -> {
+                                    auth.create("basic", BasicAuthentication.class);
+                                });
+
+                                try {
+                                    // set to SNAPSHOT as default, update in tasks.withType(PublishToMavenRepository.class)
+                                    mvn.setUrl(new URI(OSS_SNAPSHOT));
+                                } catch (URISyntaxException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                        } else {
+                            // if "ossrh.user" not provided, then publish to local
+                            repoHandler.maven(mvn -> {
+                                mvn.setName(PUBLISH_TO_LOCAL);
+                                DirectoryProperty buildDir = project.getLayout().getBuildDirectory();
+                                mvn.setUrl(buildDir.dir(LOCAL_RELEASE_DIR));
+                            });
+                        }
+                    }
                 });
 
+            }
+        });
     }
 
 }
---
+++ platforms/src/main/java/io/github/linguaphylo/platforms/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This includes Gradle plugins, which establishes a platform
+ * to use the Java Platform Module System (JPMS), >= 16,
+ * and create LPhy extensions using Service Provider Interface (SPI).
+ * It also defines LPhy extension conventions and share the build logic.
+ *
+ * @author Walter Xie
+ */
+package io.github.linguaphylo.platforms;
